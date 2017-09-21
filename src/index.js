755bcf76d0299be94f156809bb7a6998246e4004
Mac <fuji.two6@gmail.com>
2017-09-21 13:20:07 +0200

    add files

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+import style from './containers/App.css';
+
+/*ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
+*/
+ReactDOM.render(
+    React.createElement('div', {}),
+    document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/build',
+        filename: 'app.bundle.js'
+    }
+};
+module: {
+    rules: [
+        {
+            test: /\.js$/,
+            loader: "babel-loader"
+        },
+        {
+            test: /\.css$/,
+            use: [
+                { loader: 'style-loader'},
+                {
+                    loader: 'css-loader',
+                    options: {
+                        modules: true
+                    }
+                }
+            ]
+        }
+    ]
+};
